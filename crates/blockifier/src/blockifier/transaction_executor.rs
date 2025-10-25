47103e8735c8fe9360aa0ab2892b23bb0d5ab677
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2025-10-25 15:06:59 +0530

    make finalize_block public

--- crates/blockifier/src/blockifier/transaction_executor.rs
+++ crates/blockifier/src/blockifier/transaction_executor.rs
@@ -19,7 +19,7 @@ use crate::state::cached_state::{CachedState, CommitmentStateDiff, StateMaps, Tr
 use crate::state::compiled_class_hash_migration::CompiledClassHashMigrationUpdater;
 use crate::state::errors::StateError;
 use crate::state::state_api::{StateReader, StateResult};
-use crate::state::stateful_compression::{allocate_aliases_in_storage, compress, CompressionError};
+use crate::state::stateful_compression::{CompressionError, allocate_aliases_in_storage, compress};
 use crate::transaction::errors::TransactionExecutionError;
 use crate::transaction::objects::TransactionExecutionInfo;
 use crate::transaction::transaction_execution::Transaction;
@@ -230,7 +230,7 @@ fn lock_bouncer(bouncer: &Arc<Mutex<Bouncer>>) -> MutexGuard<'_, Bouncer> {
 
 /// Finalizes the creation of a block.
 /// Returns the state diff and the block weights.
-pub(crate) fn finalize_block<S: StateReader>(
+pub fn finalize_block<S: StateReader>(
     bouncer: &Arc<Mutex<Bouncer>>,
     block_state: &mut CachedState<S>,
     block_context: &BlockContext,
